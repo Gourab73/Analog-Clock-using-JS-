bd9cc6734401f8dc60b56eba20851b24cd137f50
Gourab <sarkargourab77@gmail.com>
2021-06-02 13:33:04 +0530

    ANALOG CLOCK

---
+++ Clock(using JS)/clock.js
@@ -0,0 +1,16 @@
+setInterval(() => {
+
+    t = new Date();
+    htime = t.getHours();
+    mtime = t.getMinutes();
+    stime = t.getSeconds();
+
+    hrotation = 30*htime + mtime/2;
+    mrotation = 6*mtime;
+    srotation = 6*stime;
+
+    hour.style.transform = `rotate(${hrotation}deg)`;
+    minute.style.transform = `rotate(${mrotation}deg)`;
+    second.style.transform = `rotate(${srotation}deg)`;
+
+}, 1000);
